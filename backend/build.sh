d7c0126c0478f63bfca34964e4bc9d11afabb7b4
Luis Medina <Luis_medina124@hotmail.com>
2025-12-14 02:52:22 -0400

    auto crear super usaurio

--- backend/build.sh
+++ backend/build.sh
@@ -6,3 +6,7 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# --- LÍNEA MÁGICA PARA CREAR SUPERUSUARIO ---
+# Esto crea un usuario 'admin' con contraseña 'admin123' si no existe
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'admin123')"
